99fdbc4bd53970f739c70cf6a1f2d47dde4c539c
Henry Chung <h23chung@uwaterloo.ca>
2015-05-03 18:12:23 -0400

    Added subsubsection ast

--- marktex.rb
+++ marktex.rb
@@ -6,12 +6,13 @@ input =
 #  I'm a section
  # I am not a section
 ## I am totally a SUBsection
+### I am totally a SUBSUBsection
 """
 
 @input_lines = input.split("\n")
 
 def header_parse()
-  [Subsection, Section].each do |expr|
+  [Subsubsection, Subsection, Section].each do |expr|
     child = expr.parse @input_lines[0]
 
     next if child.nil?
