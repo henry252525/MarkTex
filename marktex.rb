24906fc08db37c0ac3242f0f73c8c054b8abaf64
Elijah Andrews <elijahcandrews@gmail.com>
2015-05-04 19:14:35 -0400

    Change parsing functinos to take input as parameter

--- marktex.rb
+++ marktex.rb
@@ -1,20 +1,20 @@
 Dir[File.dirname(__FILE__) + '/ast/*.rb'].each {|file| require file }
 
-@input_lines = $stdin.read.split("\n")
+input_lines = $stdin.read.split("\n")
 
-def block_parse()
-  child = header_parse || paragraph_parse
+def block_parse(input)
+  child = header_parse(input) || paragraph_parse(input)
   return nil if child.nil?
   Block.new child
 end
 
-def header_parse()
+def header_parse(input)
   [Subsubsection, Subsection, Section].each do |expr|
-    child = expr.parse @input_lines[0]
+    child = expr.parse input[0]
 
     next if child.nil?
 
-    @input_lines.shift
+    input.shift
     return Header.new child
   end
 
@@ -24,30 +24,33 @@ end
 FORBIDDEN_STRINGS = [
   '#'
 ]
-def paragraph_parse()
+def paragraph_parse(input)
   data = []
-  until @input_lines.empty? do
-    if @input_lines.first.empty?
-      @input_lines.shift
+  until input.empty? do
+    if input.first.empty?
+      input.shift
       break
     end
     should_break = false
     FORBIDDEN_STRINGS.each do |s|
-      should_break = @input_lines.first.start_with? s
+      should_break = input.first.start_with? s
     end
     break if should_break
-    data.push @input_lines.shift
+    data.push input.shift
   end
 
   return nil if data.empty?
   Paragraph.new data.join(' ')
 end
 
-until @input_lines.empty? do
-  prev_output = block_parse
+
+
+
+until input_lines.empty? do
+  prev_output = block_parse input_lines
   if prev_output.nil?
     puts "could not parse"
-    @input_lines.shift
+    input_lines.shift
   else
     puts prev_output
   end
