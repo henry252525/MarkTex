1d9f5159d149199bd111d9a0ef491ec9e7efbd5e
Elijah Andrews <elijahcandrews@gmail.com>
2015-05-04 21:22:10 -0400

    Make all tests specify test cases in  *.in and *.out files

--- tests.rb
+++ tests.rb
@@ -1,25 +1,15 @@
 require 'test/unit'
 
 class MarkTex < Test::Unit::TestCase
+
+  # ================== HELPERS =======================
+
+
+  #TODO: Figure out if chomping is actually hiding some bugs
+  #      (try to remove chomp and see if it breaks everything)
+
   def execute(test_input)
     `echo "#{test_input}" | ruby marktex.rb`.chomp
-  end
-
-  def test_headers
-    assert_equal execute('#Section'), '\section{Section}'
-    assert_equal execute('# Section'), '\section{Section}'
-    assert_equal execute('## Subsection'), '\subsection{Subsection}'
-  end
-
-  def test_paragraph
-    assert_equal(
-      "I am a paragraph. I am on the same paragraph",
-      execute("I am a paragraph.\nI am on the same paragraph")
-    )
-    assert_equal(
-      "I am a paragraph.\nI am on another paragraph",
-      execute("I am a paragraph.\n\nI am on another paragraph")
-    )
   end
 
   def read_file_contents path
@@ -29,15 +19,15 @@ class MarkTex < Test::Unit::TestCase
     contents
   end
 
-  def test_unordered_list
-    input_files = Dir.glob 'test_cases/unordered_list/*.in'
-    output_files = Dir.glob 'test_cases/unordered_list/*.out'
+  def execute_test_dir test_case_dir
+    input_files = Dir.glob "test_cases/#{test_case_dir}/*.in"
+    output_files = Dir.glob "test_cases/#{test_case_dir}/*.out"
     test_cases = input_files.zip output_files
 
     test_cases.each do |test_case|
       input_file_path, output_file_path = test_case
-      input_data = read_file_contents input_file_path
-      output_data = read_file_contents output_file_path
+      input_data = read_file_contents(input_file_path).chomp
+      output_data = read_file_contents(output_file_path).chomp
 
       assert_equal(
         output_data,
@@ -46,6 +36,18 @@ class MarkTex < Test::Unit::TestCase
     end
   end
 
+  # ================ TEST CASES ====================
 
+  def test_unordered_list
+    execute_test_dir 'unordered_list'
+  end
+
+  def test_headers
+    execute_test_dir 'header'
+  end
+
+  def test_paragraph
+    execute_test_dir 'paragraph'
+  end
 
 end
