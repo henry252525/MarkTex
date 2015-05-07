a77da532301a4989695c1144201b91fab5c6f7e2
Henry Chung <h23chung@uwaterloo.ca>
2015-05-07 13:14:14 -0400

    Moved tests to lib as well. Will later replace with rspec

---
+++ lib/tests.rb
@@ -0,0 +1,53 @@
+require 'test/unit'
+
+class MarkTex < Test::Unit::TestCase
+
+  # ================== HELPERS =======================
+
+
+  #TODO: Figure out if chomping is actually hiding some bugs
+  #      (try to remove chomp and see if it breaks everything)
+
+  def execute_body(test_input)
+    `echo "#{test_input}" | ruby marktex.rb -b`.chomp
+  end
+
+  def read_file_contents path
+    file = File.open path, 'rb'
+    contents = file.read
+    file.close
+    contents
+  end
+
+  def execute_body_test_dir test_case_dir
+    input_files = Dir.glob "test_cases/#{test_case_dir}/*.in"
+    output_files = Dir.glob "test_cases/#{test_case_dir}/*.out"
+    test_cases = input_files.zip output_files
+
+    test_cases.each do |test_case|
+      input_file_path, output_file_path = test_case
+      input_data = read_file_contents(input_file_path).chomp
+      output_data = read_file_contents(output_file_path).chomp
+
+      assert_equal(
+        output_data,
+        execute_body(input_data)
+      )
+    end
+  end
+
+  # ================ TEST CASES ====================
+
+  def test_unordered_list
+    execute_body_test_dir 'unordered_list'
+  end
+
+  def test_headers
+    execute_body_test_dir 'header'
+  end
+
+  def test_paragraph
+    execute_body_test_dir 'paragraph'
+  end
+
+end
